b6bab1de88d6063c1b4908d0441ae7723794d6cf
Paolo de Dios <paolodedios@gmail.com>
2012-10-21 18:39:40 -0500

    Refactored ido mode config to its own init file

---
+++ .emacs.d/custom/i-do.el
@@ -0,0 +1,35 @@
+;; -*-emacs-lisp-*-
+;;
+;; IDO mode
+;;
+;; Paolo de Dios <paolodedios@gmail.com>
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable IDO mode for everything
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'ido)
+
+(ido-mode                                     t)
+(setq ido-everywhere                          t)
+(setq ido-enable-flex-matching                t)
+(setq ido-case-fold                           t)
+(setq ido-use-filename-at-point          'guess)
+(setq ido-enable-prefix                     nil)
+(setq ido-max-prospects                      10)
+
+;; ido file listing order
+(setq ido-file-extensions-order
+      '(".java"
+        ".js"
+        ".css"
+        ".html"
+        ".xml"
+        ".c"
+        ".cpp"
+        ".h"
+        ".el"
+        )
+      )
+
