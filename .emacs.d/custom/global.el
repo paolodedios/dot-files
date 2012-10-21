9ecae6c7868f543743ff70309b42be11e84388ae
Paolo de Dios <paolodedios@gmail.com>
2012-10-20 20:50:23 -0400

    Customized Emacs 23 linum mode

--- .emacs.d/custom/global.el
+++ .emacs.d/custom/global.el
@@ -115,19 +115,75 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Turn on line/column number modes
+;; @see http://www.emacswiki.org/LineNumbers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'linum)
 
-(line-number-mode                             1)
+;; Enable linum for all modes except those on this list
+(setq linum-disabled-modes-list
+      '(eshell-mode
+        wl-summary-mode
+        compilation-mode
+        )
+      )
+
+(defun linum-on ()
+  (unless (or (minibufferp) (member major-mode linum-disabled-modes-list))
+    (linum-mode 1)
+    )
+  )
+
 (column-number-mode                           t)
 
 ;; enable global linum-mode if not already toggled on
 (if (equal linum-mode nil)
-    (global-linum-mode))
+    (global-linum-mode)
+  )
 
 (fringe-mode                                  0)
 
+;; Set linum-format to pad for up to 4 digit line numbers, followed by a space
+;; then a solid '|' character and then another space
+(setq linum-format                "%4d \u2502 ")
+
+;; Select lines by click dragging on the margin
+(defvar *linum-mdown-line* nil)
+
+(defun line-at-click ()
+  (save-excursion
+	(let ((click-y (cdr (cdr (mouse-position))))
+		  (line-move-visual-store line-move-visual))
+	  (setq line-move-visual t)
+	  (goto-char (window-start))
+	  (next-line (1- click-y))
+	  (setq line-move-visual line-move-visual-store)
+	  ;; If you are using tabbar substitute the next line with
+	  ;; (line-number-at-pos))))
+	  (1+ (line-number-at-pos)))))
+
+(defun md-select-linum ()
+  (interactive)
+  (goto-line (line-at-click))
+  (set-mark (point))
+  (setq *linum-mdown-line*
+		(line-number-at-pos)))
+
+(defun mu-select-linum ()
+  (interactive)
+  (when *linum-mdown-line*
+	(let (mu-line)
+	  ;; (goto-line (line-at-click))
+	  (setq mu-line (line-at-click))
+	  (goto-line (max *linum-mdown-line* mu-line))
+	  (set-mark (line-end-position))
+	  (goto-line (min *linum-mdown-line* mu-line))
+	  (setq *linum-mdown*
+			nil))))
+
+(global-set-key (kbd "<left-margin> <down-mouse-1>")  'md-select-linum)
+(global-set-key (kbd "<left-margin> <mouse-1>")       'mu-select-linum)
+(global-set-key (kbd "<left-margin> <drag-mouse-1>")  'mu-select-linum)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Suppress startup messages
