7038c4fb20ade5bde863f0e9c980ae2c24728d31
Paolo de Dios <paolodedios@gmail.com>
2013-01-02 00:29:38 -0600

    Cleanup groovy mode

--- .emacs.d/custom/groovy.el
+++ .emacs.d/custom/groovy.el
@@ -6,9 +6,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(autoload 'groovy-mode     "groovy-mode" "Groovy Editing Mode"                              t)
-(autoload 'run-groovy      "inf-groovy"  "Run an inferior Groovy process"                    )
-(autoload 'inf-groovy-keys "inf-groovy"  "Set local key defs for inf-groovy in groovy-mode"  )
+(require 'groovy-electric)
+
+(autoload 'groovy-mode     "groovy-mode" "Groovy Editing Mode"   t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Groovy hook section, called on entry of Groovy mode
@@ -16,6 +16,7 @@
 
 (add-hook 'groovy-mode-hook
           '(lambda ()
+             (groovy-electric-mode                   1)
              ;; toggle major mode editor options
              (c-toggle-auto-state                    1)
              (c-toggle-hungry-state                  1)
@@ -26,7 +27,6 @@
              (setq c-basic-offset                    4)
              (setq tab-width                         4)
              (setq indent-tabs-mode                nil)
-             (inf-groovy-keys                         )
              ;; set programming style
              (c-add-style "my-groovy-programming-style" my-java-mode-programming-style t)
              (c-set-style "my-groovy-programming-style")
