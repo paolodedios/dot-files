0eabfbae1a0835820bed39699d74ca6213337af8
Paolo de Dios <paolodedios@gmail.com>
2013-05-19 02:31:23 -0500

    Added buffer list highlighting for electric buffer list

--- .emacs.d/custom/theme.el
+++ .emacs.d/custom/theme.el
@@ -141,6 +141,33 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set buffer menu highlighting
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq buffer-menu-buffer-font-lock-keywords
+      '(("^....[*]Man .*Man.*"   . font-lock-variable-name-face) ; Man page
+        (".*Dired.*"             . font-lock-comment-face)       ; Dired
+        ("^....[*]shell.*"       . font-lock-preprocessor-face)  ; shell buff
+        (".*[*]scratch[*].*"     . font-lock-function-name-face) ; scratch buffer
+        ("^....[*].*"            . font-lock-string-face)        ; "*" named buffers
+        ("^..[*].*"              . font-lock-constant-face)      ; Modified
+        ("^.[%].*"               . font-lock-keyword-face)))     ; Read only
+
+
+(defun buffer-menu-custom-font-lock  ()
+  (let ((font-lock-unfontify-region-function
+         (lambda (start end)
+           (remove-text-properties start end '(font-lock-face nil)))))
+    (font-lock-unfontify-buffer)
+    (set (make-local-variable 'font-lock-defaults)
+         '(buffer-menu-buffer-font-lock-keywords t))
+    (font-lock-fontify-buffer)
+    )
+  )
+
+(add-hook 'electric-buffer-menu-mode-hook 'buffer-menu-custom-font-lock)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set the max buffer size (in bytes) before fontification is suppressed
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
