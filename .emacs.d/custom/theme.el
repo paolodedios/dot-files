1618fef44439e435c138e71fd0caf975f8169053
Paolo de Dios <paolodedios@gmail.com>
2013-05-19 20:07:34 -0500

    Updated formatting

--- .emacs.d/custom/theme.el
+++ .emacs.d/custom/theme.el
@@ -151,16 +151,22 @@
         (".*[*]scratch[*].*"     . font-lock-function-name-face) ; scratch buffer
         ("^....[*].*"            . font-lock-string-face)        ; "*" named buffers
         ("^..[*].*"              . font-lock-constant-face)      ; Modified
-        ("^.[%].*"               . font-lock-keyword-face)))     ; Read only
+        ("^.[%].*"               . font-lock-keyword-face)       ; Read only
+        )
+      )
 
 
 (defun buffer-menu-custom-font-lock  ()
   (let ((font-lock-unfontify-region-function
          (lambda (start end)
-           (remove-text-properties start end '(font-lock-face nil)))))
+           (remove-text-properties start end '(font-lock-face nil))
+           )
+         )
+        )
     (font-lock-unfontify-buffer)
     (set (make-local-variable 'font-lock-defaults)
-         '(buffer-menu-buffer-font-lock-keywords t))
+         '(buffer-menu-buffer-font-lock-keywords t)
+         )
     (font-lock-fontify-buffer)
     )
   )
