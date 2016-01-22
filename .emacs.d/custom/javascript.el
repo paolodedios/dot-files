0497d3132464acb454f9e7eb67bd547c824f051c
Paolo de Dios <paolodedios@gmail.com>
2016-01-22 16:22:31 -0600

    Updated js2-mode configuration. Relocated javascript-mode customizations for MMM to mmm.el.

--- .emacs.d/custom/javascript.el
+++ .emacs.d/custom/javascript.el
@@ -6,12 +6,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; js2-mode package for .js file editing.
-;l @see https://github.com/mooz/js2-mode
+;; Enhanced JSON mode for syntax highlighting
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'json-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enhanced js2-mode package for .js file editing.
+;;
+;; @see https://github.com/mooz/js2-mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (autoload 'js2-mode "js2-mode" nil t)
-(require 'json)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; js2-mode imenu support
@@ -38,90 +44,8 @@
 (require 'js2-highlight-vars)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Javascript style
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defconst my-javascript-mode-programming-style
-  ;; hanging brace setup
-  '((c-hanging-braces-alist .
-                            ((brace-list-open                    after)
-                             (brace-entry-open                   after)
-                             (substatement-open                 before)
-                             (block-close            . c-snug-do-while)
-                             (extern-lang-open                   after)
-                             (inexpr-class-open                  after)
-                             (inexpr-class-close                before)
-                             ))
-    ;; cleanup shortcuts
-    (c-cleanup-list         .
-                            ((brace-else-brace                        )
-                             (brace-elseif-brace                      )
-                             (brace-catch-brace                       )
-                             (list-close-comma                        )
-                             ))
-    ;; indentation offsets
-    (c-offsets-alist        .
-                            ((access-label                         . 0)
-                             (inline-open                          . 0)
-                             (substatement-open                    . 0)
-                             (statement-block-intro                . +)
-                             (block-close                          . 0)
-                             (do-while-closure                     . 0)
-                             (case-label                           . +)
-                             (statement-case-intro                 . +)
-                             (statement-cont c-lineup-cascaded-calls +)
-                             (stream-op                            . c-lineup-streamop)
-                             ))
-    (c-lineup-math                   1)
-    (c-lineup-inexpr-block           1)
-
-    ) "My Javascript Programming Style")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; js2-mode style
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; With regular JS2-mode, pressing TAB at the beginning of a line won't move the
-;; caret to the first non-space character if the line is already properly. This
-;; indentation function fixes that.
-
-(defun my-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (js--proper-indentation parse-status))
-           node
-           )
-
-      (save-excursion
-
-        ;; I like to indent case and labels to half of the tab width
-        (back-to-indentation)
-        (if (looking-at "case\\s-")
-            (setq indentation (+ indentation (/ js-indent-level 2)))
-          )
-
-        ;; consecutive declarations in a var statement are nice if
-        ;; properly aligned, i.e:
-        ;;
-        ;; var foo = "bar",
-        ;;     bar = "foo";
-        (setq node (js2-node-at-point))
-        (when (and node
-                   (= js2-NAME (js2-node-type node))
-                   (= js2-VAR (js2-node-type (js2-node-parent node))))
-          (setq indentation (+ 4 indentation)))
-        )
-
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset))
-
-      )
-    )
-  )
 
 ;; With c-mode there is a key binding M-C-q which indents the block starting with
 ;; the paren under the cursor.  The following function implements this generically
@@ -166,9 +90,9 @@
 (add-hook 'js2-mode-hook
           '(lambda ()
              (require 'js)
-             ;; toggle major mode editor options
-             ;; unlike other major modes, js2-mode is not
-             ;; a cc-mode derivative
+             (setq mode-name                     "js2")
+             ;; toggle major mode editor options; unlike other major modes,
+             ;; js2-mode is not a cc-mode derivative
              (c-toggle-auto-state                    1)
              (c-toggle-hungry-state                  1)
              (auto-fill-mode                         1)
@@ -179,34 +103,74 @@
              (setq tab-width                         4)
              (setq indent-tabs-mode                nil)
 
+             ;; js2-mode specific options
              (setq js2-use-font-lock-faces           t)
-             (setq js2-highlight-level               4)
+             (setq js2-highlight-level               3)
+             (setq js2-basic-offset                  4)
+             (setq js2-idle-timer-delay            0.1)
 
-             ;; additional js2-mode indent sexp
-             ;; incompatible with js2-mode from https://github.com/mooz/js2-mode
-             ;; (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
+             (setq js2-global-externs '("module"
+                                        "require"
+                                        "assert"
+                                        "setTimeout"
+                                        "clearTimeout"
+                                        "setInterval"
+                                        "clearInterval"
+                                        "location"
+                                        "__dirname"
+                                        "console"
+                                        "JSON"
+                                        )
+                   )
 
-             ;; enable highlight-vars-mode
-             (if (featurep 'js2-highlight-vars)  (js2-highlight-vars-mode))
+             ;; delegate syntax checking to external program like flycheck
+             (setq js2-show-parse-errors                    nil)
+             (setq js2-strict-missing-semi-warning          nil)
+             (setq js2-strict-trailing-comma-warning          t)
+             (setq js2-missing-semi-one-line-override         t)
+
+             ;; indentation options for Emacs 24.5 and earlier.
+             ;; all other indentation code has been ported to
+             ;; js-mode for Emacs 25 and later.
+             ;;
+             ;; @see https://github.com/mooz/js2-mode/blob/master/js2-old-indent.el
+             ;; for additional commentary on identation implementation.
+             ;;
+             ;; @see https://lists.gnu.org/archive/html/emacs-elpa-diffs/2015-07/msg00047.html
+             ;; for commits migrating from js2-indent to js-indent
+             ;;
+             ;; @se https://github.com/emacs-mirror/emacs/blob/master/lisp/progmodes/js.el
+             ;; for list of new and migrated indent options
+             (setq js2-indent-switch-body                     t)
+             (setq js2-pretty-multiline-declarations   'dynamic)
+
+             ;; enable highlight-vars-mode, which automatically highlights
+             ;; all occurrences of the variable under the cursor within its
+             ;; defining scope
+             (if (featurep 'js2-highlight-vars)
+                 (js2-highlight-vars-mode)
+               )
 
              ;; bind buffer local keys
-             (define-key js2-mode-map [(meta control |)]      'cperl-lineup          )
-             (define-key js2-mode-map [(meta control \;)]
+             (define-key js2-mode-map [(control meta |)]      'cperl-lineup             )
+             (define-key js2-mode-map [return]                'newline-and-indent       )
+             (define-key js2-mode-map [backspace]             'c-electric-backspace     )
+             (define-key js2-mode-map [(control d)]           'c-electric-delete-forward)
+             (define-key js2-mode-map [(control meta q)]      'my-js2-indent-sexp       )
+             (define-key js2-mode-map [(control meta \;)]
                '(lambda()
                   (interactive)
-                  (insert "/* -----[ ")
+                  (insert "/* ")
                   (save-excursion
-                    (insert " ]----- */"))
+                    (insert "  */"))
                   ))
-             (define-key js2-mode-map [return]             'newline-and-indent       )
-             (define-key js2-mode-map [backspace]          'c-electric-backspace     )
-             (define-key js2-mode-map [(control d)]        'c-electric-delete-forward)
-             (define-key js2-mode-map [(control meta q)]   'my-js2-indent-sexp       )
              )
           )
 
 
-(add-hook 'js-mode-hook  'js2-minor-mode)
+;; Instead of using js2 as a major mode, it can optionally be
+;; loaded as a minor mode to js-mode
+;; (add-hook 'js-mode-hook  'js2-minor-mode)
 
 ;; @see https://github.com/ScottyB/ac-js2
 ;; (add-hook 'js2-mode-hook 'ac-js2-mode   )
@@ -214,6 +178,24 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Turn on font-lock
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Replace anonymous function declarations with the lambda symbol.
+;;
+;; (font-lock-add-keywords
+;;  'js2-mode `(("\\<\\(function\\) *("
+;;               (0 (progn (compose-region (match-beginning 1)
+;;                                         (match-end 1) "\u0192")
+;;                         nil))))
+;;  )
+
+;; Replace return keyword in one-line functions with the right arrow symbol.
+;;
+;; (font-lock-add-keywords
+;;  'js2-mode `(("function *([^)]*) *{ *\\(return\\) "
+;;               (0 (progn (compose-region (match-beginning 1)
+;;                                         (match-end 1) "\u2190")
+;;                         nil))))
+;; )
 
 (add-hook 'js2-mode-user-hook     'turn-on-font-lock)
 (add-hook 'javascript-mode-hook   'turn-on-font-lock)
@@ -224,7 +206,7 @@
 
 (setq auto-mode-alist (append '(("\\.js$"       . js2-mode          )) auto-mode-alist))
 (setq auto-mode-alist (append '(("\\.jsx$"      . js2-mode          )) auto-mode-alist))
-(setq auto-mode-alist (append '(("\\.json$"     . js-mode           )) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.json$"     . json-mode         )) auto-mode-alist))
 
 ;; add node-js to interpreter list
 (add-to-list 'interpreter-mode-alist '("node"   . js2-mode))
