efed38ee46126c993fea8743d3fa79615bd2c2fe
Paolo de Dios <paolodedios@gmail.com>
2017-03-21 23:32:15 -0700

    Added some customizations and fixed typos

--- .emacs.d/modules/config-typescript-mode.el
+++ .emacs.d/modules/config-typescript-mode.el
@@ -8,7 +8,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Go mode
+;; Typescript mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'typescript-mode)
@@ -20,29 +20,42 @@
 
 (defun setup-tide-mode ()
   (interactive)
-  (tide-setup)
+  (tide-setup                    )
   (eldoc-mode                  +1)
   (tide-hl-identifier-mode     +1)
 
   (flycheck-mode               +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
 
-  ;; company is an optional dependency. You have to
+  ;; Company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
   (company-mode               +1)
   )
 
-;; aligns annotation to the right hand side
+;; Align annotation to the right hand side
 (setq company-tooltip-align-annotations  t)
 
-;; formats the buffer before saving
+;; Format the buffer before saving
 (add-hook 'before-save-hook 'tide-format-before-save)
 
-(add-hook 'typescript-mode-hook  'turn-on-font-lock)
-
+;; Setup tide-mode using the above initializer fun
 (add-hook 'typescript-mode-hook  #'setup-tide-mode)
 
+;; Add ts-comint key bindings for sending things to the Typescript
+;; interpreter inside of typescript-mode
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-x C-e")  'ts-send-last-sexp       )
+            (local-set-key (kbd "C-M-x"  )  'ts-send-last-sexp-and-go)
+            (local-set-key (kbd "C-c b"  )  'ts-send-buffer          )
+            (local-set-key (kbd "C-c C-b")  'ts-send-buffer-and-go   )
+            (local-set-key (kbd "C-c l"  )  'ts-load-file-and-go     )
+            )
+          )
+
+
+(add-hook 'typescript-mode-hook  'turn-on-font-lock)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; File associations
