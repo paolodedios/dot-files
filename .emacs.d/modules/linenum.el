1e62515759230819b51dd9288f5aa6f266cdcc88
Paolo de Dios <paolodedios@gmail.com>
2017-06-23 13:07:32 -0700

    Added implementation of line wrap overlay to ensure linum face is consistent

--- .emacs.d/modules/linenum.el
+++ .emacs.d/modules/linenum.el
@@ -22,6 +22,12 @@
 (column-number-mode                           t)
 (fringe-mode                                  0)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Line number formatting
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;
 ;; Set linum-format to pad for up to 4 digit line numbers, followed by a space
 ;; then a solid '|' character and then another space
 ;;
@@ -32,8 +38,7 @@
 ;; FIGURE SPACE, a space that always has the same width as a digit to ensure
 ;; that whitespace-mode does not pickup any formatting from linum-mode
 ;;
-(add-hook 'linum-before-numbering-hook
-          (lambda ()
+(defun linum-format-number-padding ()
             (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
               (setq linum-format
                     `(lambda (line)
@@ -47,14 +52,68 @@
                                     "\u2502"
                                     "\u2007"
                                     )
-                                   'face 'linum)
-                       )
-                    )
+                                   'face 'linum) ))
               )
             )
-          )
 
+(add-hook 'linum-before-numbering-hook 'linum-format-number-padding)
+
+;;
+;; Set padding characters to overlay line numbering against when line wrapping
+;; is encountered.
+;;
+;; @see https://emacs.stackexchange.com/questions/805/
+;;        how-can-i-fill-the-entire-line-number-bar-with-a-custom-background-color
+;;
+(defvar linum-linewrap-margin-display
+  `((margin left-margin) ,(propertize (concat
+                "\u2007"
+                "\u2007"
+                "\u2007"
+                ":"
+                "\u2007"
+                "\u2502"
+                "\u2007"
+                ) 'face 'linum))
+  "String used on the margin.")
+
+(defvar-local linum-linewrap-margin-overlays nil
+  "List of overlays in current buffer.")
+
+(defun linum-linewrap-make-overlay-at (p)
+  "Create a margin overlay at position P."
+  (push (make-overlay p (1+ p)) linum-linewrap-margin-overlays)
+  (overlay-put
+   (car linum-linewrap-margin-overlays) 'before-string
+   (propertize " "  'display linum-linewrap-margin-display)))
+
+(defun linum-linewrap-setup-margin-overlays ()
+  "Put overlays on each line which is visually wrapped."
+  (interactive)
+  (let ((ww (- (window-width)
+               (if (= 0 (or (cdr fringe-mode) 1)) 1 0)))
+        ov)
+    (mapc #'delete-overlay linum-linewrap-margin-overlays)
+    (save-excursion
+      (goto-char (point-min))
+      (while (null (eobp))
+        ;; On each logical line
+        (forward-line 1)
+        (save-excursion
+          (forward-char -1)
+          ;; Check if it has multiple visual lines.
+          (while (>= (current-column) ww)
+            (linum-linewrap-make-overlay-at (point))
+            (forward-char (- ww))))))))
+
+
+;; Run whenever linum-mode updates the line numbers
+(add-hook 'linum-before-numbering-hook 'linum-linewrap-setup-margin-overlays)
+
+
+;;
 ;; Select lines by click dragging on the margin
+;;
 (defvar *linum-mdown-line* nil)
 
 (defun line-at-click ()
@@ -94,6 +153,11 @@
       )
     )
   )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Keyboard/mouse bindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (global-set-key (kbd "<left-margin> <down-mouse-1>")  'md-select-linum)
 (global-set-key (kbd "<left-margin> <mouse-1>")       'mu-select-linum)
