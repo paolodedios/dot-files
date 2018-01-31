0a89f004102a5ba3769ff132b0fc1848f2133f88
Paolo de Dios <paolodedios@gmail.com>
2018-01-31 13:27:37 -0800

    Migrate showcolors function to functions.bash since it is not theme related

--- .bash.d/custom/functions.bash
+++ .bash.d/custom/functions.bash
@@ -724,6 +724,27 @@ function showfuns()
 }
 
 ########################################################################################
+# Display terminal color constants
+########################################################################################
+
+function showcolors()
+{
+    echo
+    echo -e "$(tput bold) reg  bld  und   tput-command-colors$(tput sgr0)"
+
+    for i in $(seq 1 256); do
+        echo " $(tput setaf $i)Text$(tput sgr0) $(tput bold)"`
+            `"$(tput setaf $i)Text$(tput sgr0) $(tput sgr 0 1)"`
+            `"$(tput setaf $i)Text$(tput sgr0)  \$(tput setaf $i)"
+    done
+
+    echo ' Bold            $(tput bold)'
+    echo ' Underline       $(tput sgr 0 1)'
+    echo ' Reset           $(tput sgr0)'
+    echo
+}
+
+########################################################################################
 # Define OS specific functions
 ########################################################################################
 
