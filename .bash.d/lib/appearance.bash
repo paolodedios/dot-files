2cc05114a4831b863880a9c9f62f620a31a30b4a
Paolo de Dios <paolodedios@gmail.com>
2018-01-30 20:13:37 -0800

    Namespaced CUSTOM_THEME_DIR environment variable to BASH_IT_CUSTOM_THEME_DIR

--- .bash.d/lib/appearance.bash
+++ .bash.d/lib/appearance.bash
@@ -2,19 +2,29 @@
 #
 ########################################################################################
 
-# colored ls
+########################################################################################
+# Colored ls
+########################################################################################
+
 export LSCOLORS='Gxfxcxdxdxegedabagacad'
 
-if [[ -z "$CUSTOM_THEME_DIR" ]]; then
-    CUSTOM_THEME_DIR="${BASH_IT_CUSTOM:=${BASH_IT}/custom}/themes"
+########################################################################################
+# Set custom theme directory
+########################################################################################
+
+if [[ -z "$BASH_IT_CUSTOM_THEME_DIR" ]]; then
+    BASH_IT_CUSTOM_THEME_DIR="${BASH_IT_CUSTOM:=${BASH_IT}/custom}/themes"
 fi
 
+########################################################################################
 # Load the theme
+########################################################################################
+
 if [[ $BASH_IT_THEME ]]; then
     if [[ -f $BASH_IT_THEME ]]; then
         source $BASH_IT_THEME
-    elif [[ -f "$CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash" ]]; then
-        source "$CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
+    elif [[ -f "$BASH_IT_CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash" ]]; then
+        source "$BASH_IT_CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
     else
         source "$BASH_IT/themes/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
     fi
