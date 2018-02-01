f1370b78669fde7a14854f036e564c6be15471e4
Paolo de Dios <paolodedios@gmail.com>
2018-02-01 10:43:06 -0800

    Migrated macOS specific aliases to macos.aliases.bash

--- .bash.d/aliases/available/macos.aliases.bash
+++ .bash.d/aliases/available/macos.aliases.bash
@@ -8,18 +8,19 @@ about-alias 'macOS specific abbreviations'
 
 case $OSTYPE in
     darwin*)
-        # Desktop Programs
+        # Launch common desktop programs from the command line
         alias preview="open -a '$PREVIEW'"
-        alias xcode="open -a '/Applications/XCode.app'"
-        alias safari="open -a safari"
-        alias firefox="open -a firefox"
-        alias chrome="open -a google\ chrome"
-        alias chromium="open -a chromium"
         alias f='open -a Finder '
         alias fh='open -a Finder .'
         alias textedit='open -a TextEdit'
         alias hex='open -a "Hex Fiend"'
         alias subl='open -a Sublime\ Text'
+
+        alias xcode="open -a '/Applications/XCode.app'"
+        alias safari="open -a safari"
+        alias firefox="open -a firefox"
+        alias chrome="open -a google\ chrome"
+        alias chromium="open -a chromium"
 
         # Requires growlnotify, which can be found in the Growl DMG under "Extras"
         alias grnot='growlnotify -s -t Terminal -m "Done"'
@@ -46,5 +47,49 @@ case $OSTYPE in
         # Pin to the tail of long commands for an audible alert after long processes
         # curl http://downloads.com/hugefile.zip; lmk
         alias lmk="say 'Process complete.'"
+
+        # Clean up LaunchServices to remove duplicates in the “Open With” menu
+        alias rebuild-menu="$LS_REGISTER_PATH/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+
+        # Reboot the Finder
+        alias kill-finder="killall Finder && open /System/Library/CoreServices/Finder.app"
+
+        # Reboot the window server/manager and force a logout
+        alias kill-windowserver="sudo killall -HUP WindowServer"
+
+        # Cleanup Resource Forks
+        alias cleanresforks="find . -name \*._*|xargs \rm"
+
+        # Flush Directory Service cache
+        alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+
+        # ROT13-encode text. Works for decoding also
+        alias rot13="tr a-zA-Z n-za-mN-ZA-M"
+
+        # Hide/show all desktop icons (useful when presenting)
+        alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+        alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+        # Disable Spotlight
+        alias spotoff="sudo mdutil -a -i off"
+
+        # Enable Spotlight
+        alias spoton="sudo mdutil -a -i on"
+
+        # Show program names with lsof
+        alias slsof="sudo lsof -i -P"
+
+        # Canonical hex dump; some systems have this symlinked
+        command -v hd > /dev/null || alias hd="hexdump -C"
+
+        # OS X has no `md5sum`, so use `md5` as a fallback
+        command -v md5sum > /dev/null || alias md5sum="md5"
+
+        # OS X has no `sha1sum`, so use `shasum` as a fallback
+        command -v sha1sum > /dev/null || alias sha1sum="shasum"
+
+        # If Matlab is installed, ensure it runs properly on the command line
+        command -v matlab > /dev/null && alias matlab="matlab_console"
+        command -v matlab > /dev/null && alias matlabex="matlab_run_file"
         ;;
 esac
