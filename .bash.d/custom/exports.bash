81c482655a4ee633ff78d14b5aa1d3add377e3e6
Paolo de Dios <paolodedios@gmail.com>
2018-02-01 00:36:50 -0800

    Removed static NETIF env var

--- .bash.d/custom/exports.bash
+++ .bash.d/custom/exports.bash
@@ -15,7 +15,6 @@ export OS=$(uname -s | sed -e 's/  */-/g;y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghij
 export OSVERSION=$(uname -r); OSVERSION=$(expr "$OSVERSION" : '[^0-9]*\([0-9]*\.[0-9]*\)')
 export MACHINE=$(uname -m | sed -e 's/  */-/g;y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/')
 export PLATFORM="$MACHINE-$OS-$OSVERSION"
-export NETIF=en0
 
 ########################################################################################
 # Configure language variables
