76c72f305689a40dfee2d743c9dd4319c2f70569
Paolo de Dios <paolodedios@gmail.com>
2018-02-01 23:17:13 -0800

    Check for docker-machine on macOS before proceeding

--- .bash.d/plugins/available/docker-machine.plugin.bash
+++ .bash.d/plugins/available/docker-machine.plugin.bash
@@ -4,7 +4,7 @@ about-plugin 'Helpers to get Docker setup correctly for docker-machine'
 [ -z ${BASH_IT_DOCKER_MACHINE+x} ] && BASH_IT_DOCKER_MACHINE='dev'
 
 # Note, this might need to be different if you use a machine other than 'dev'
-if [[ `uname -s` == "Darwin" ]]; then
+if [[ `uname -s` == "Darwin" ] &&  $(type -p docker-machine) ]; then
   # check if dev machine is running
   docker-machine ls | grep --quiet "$BASH_IT_DOCKER_MACHINE.*Running"
   if [[ "$?" = "0" ]]; then
