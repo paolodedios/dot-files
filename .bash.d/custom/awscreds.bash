7e5b5fe12388b83f3cefe3794762bdcbf1a772eb
Paolo de Dios <paolodedios@gmail.com>
2019-01-02 22:31:45 -0800

    Relocated aws-vault and chamber helper functions to bottom of file

--- .bash.d/custom/awscreds.bash
+++ .bash.d/custom/awscreds.bash
@@ -6,131 +6,6 @@
 
 ################################################################################
 #
-# AWS Vault Environment Settings
-#
-# @see https://github.com/99designs/aws-vault
-#
-################################################################################
-
-case $OSTYPE in
-    darwin*)
-        #
-        # Under mac OS, aws-vault defaults to using the OS keychain, so default to
-        # osascript (native dialog) keychain password prompting instead of terminal
-        # based prompting on mac OS.
-        #
-        export AWS_VAULT_PROMPT=osascript
-        #
-        # Default to the login keychain for aws-vault.
-        #
-        export AWS_VAULT_KEYCHAIN_NAME=login
-        ;;
-    linux*)
-        #
-        # Under Linux, aws-vault defaults to using encrypted files to allow for
-        # terminal and gnome desktop usage, so use terminal based prompting.
-        #
-        export AWS_VAULT_PROMPT=terminal
-        ;;
-esac
-
-
-#
-# Launch aws-vault credentials metadata server
-#
-# Example Usage:
-#
-# $ awsvs appliedtheory-service-profile
-# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
-#
-function awsvs()
-{
-    #
-    # Default to 60 minute assume role TTLs and 12h session TTLs
-    # unless specified otherwise by environment variables.
-    #
-    local aws_assume_role_ttl="${AWS_ASSUME_ROLE_TTL:-60m}"
-    local aws_session_ttl="${AWS_SESSION_TTL:-12h}"
-
-    #
-    # Use of exec helps reduce the number of processes that are hanging around.
-    #
-    # @see https://github.com/99designs/aws-vault/blob/master/USAGE.md
-    #
-    aws-vault exec                                   \
-         --server                                    \
-         --assume-role-ttl=$aws_assume_role_ttl      \
-         --session-ttl=$aws_session_ttl              \
-         $1
-}
-
-
-#
-# Launch aws-vault execute wrapper.
-#
-# Example Usage:
-#
-# $ awsve appliedtheory-admin-profile -- aws s3 ls
-# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
-#
-function awsve()
-{
-    #
-    # Default to 60 minute assume role TTLs and 12h session TTLs
-    # unless specified otherwise by environment variables.
-    #
-    local aws_assume_role_ttl="${AWS_ASSUME_ROLE_TTL:-60m}"
-    local aws_session_ttl="${AWS_SESSION_TTL:-12h}"
-
-    #
-    # Use of exec helps reduce the number of processes that are hanging around.
-    #
-    # @see https://github.com/99designs/aws-vault/blob/master/USAGE.md
-    #
-    aws-vault exec                                   \
-         --assume-role-ttl=$aws_assume_role_ttl      \
-         --session-ttl=$aws_session_ttl              \
-         $@
-}
-
-
-#
-# Launch chamber vis-a-vis aws-vault
-#
-# 1. Example Usage (List secrets and expand secret values) :
-#
-# $ chamber appliedtheory-service-profile list -e <service-name>
-# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
-# Key         Version                  LastModified      User             Value
-# apikey      2                        06-09 17:30:56    admin            apikeyvalue
-# other       1                        06-09 17:30:34    admin            othervalue
-#
-# 2. Example Usage (Execute command with exported aws and secrets in env) :
-#
-# $ chamber appliedtheory-service-profile exec <service-name> -- <command>
-# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
-# ...
-#
-#
-function chmbre()
-{
-    #
-    # First argument must be the aws-vault profile name
-    #
-    local awsvault_profile=$1
-    #
-    # Mask profile name from argument list
-    #
-    shift
-    #
-    # Execute chamber via 'aws-vault exec'
-    #
-    awsve $awsvault_profile -- chamber $@
-}
-
-
-################################################################################
-#
 # AWS Environment Settings
 #
 ################################################################################
@@ -290,4 +165,129 @@ function load_aws_credentials()
     fi
 
     return $?
+}
+
+
+################################################################################
+#
+# AWS Vault Helpers
+#
+# @see https://github.com/99designs/aws-vault
+#
+################################################################################
+
+case $OSTYPE in
+    darwin*)
+        #
+        # Under mac OS, aws-vault defaults to using the OS keychain, so default to
+        # osascript (native dialog) keychain password prompting instead of terminal
+        # based prompting on mac OS.
+        #
+        export AWS_VAULT_PROMPT=osascript
+        #
+        # Default to the login keychain for aws-vault.
+        #
+        export AWS_VAULT_KEYCHAIN_NAME=login
+        ;;
+    linux*)
+        #
+        # Under Linux, aws-vault defaults to using encrypted files to allow for
+        # terminal and gnome desktop usage, so use terminal based prompting.
+        #
+        export AWS_VAULT_PROMPT=terminal
+        ;;
+esac
+
+
+#
+# Launch aws-vault credentials metadata server
+#
+# Example Usage:
+#
+# $ awsvs appliedtheory-service-profile
+# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
+#
+function awsvs()
+{
+    #
+    # Default to 60 minute assume role TTLs and 12h session TTLs
+    # unless specified otherwise by environment variables.
+    #
+    local aws_assume_role_ttl="${AWS_ASSUME_ROLE_TTL:-60m}"
+    local aws_session_ttl="${AWS_SESSION_TTL:-12h}"
+
+    #
+    # Use of exec helps reduce the number of processes that are hanging around.
+    #
+    # @see https://github.com/99designs/aws-vault/blob/master/USAGE.md
+    #
+    aws-vault exec                                   \
+         --server                                    \
+         --assume-role-ttl=$aws_assume_role_ttl      \
+         --session-ttl=$aws_session_ttl              \
+         $1
+}
+
+
+#
+# Launch aws-vault execute wrapper.
+#
+# Example Usage:
+#
+# $ awsve appliedtheory-admin-profile -- aws s3 ls
+# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
+#
+function awsve()
+{
+    #
+    # Default to 60 minute assume role TTLs and 12h session TTLs
+    # unless specified otherwise by environment variables.
+    #
+    local aws_assume_role_ttl="${AWS_ASSUME_ROLE_TTL:-60m}"
+    local aws_session_ttl="${AWS_SESSION_TTL:-12h}"
+
+    #
+    # Use of exec helps reduce the number of processes that are hanging around.
+    #
+    # @see https://github.com/99designs/aws-vault/blob/master/USAGE.md
+    #
+    aws-vault exec                                   \
+         --assume-role-ttl=$aws_assume_role_ttl      \
+         --session-ttl=$aws_session_ttl              \
+         $@
+}
+
+
+#
+# Launch chamber vis-a-vis aws-vault
+#
+# 1. Example Usage (List secrets and expand secret values) :
+#
+# $ chamber appliedtheory-service-profile list -e <service-name>
+# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
+# Key         Version                  LastModified      User             Value
+# apikey      2                        06-09 17:30:56    admin            apikeyvalue
+# other       1                        06-09 17:30:34    admin            othervalue
+#
+# 2. Example Usage (Execute command with exported aws and secrets in env) :
+#
+# $ chamber appliedtheory-service-profile exec <service-name> -- <command>
+# Enter token for arn:aws:iam::23456789101:mfa/iam_user: 549236
+# ...
+#
+#
+function chmbre()
+{
+    #
+    # First argument must be the aws-vault profile name
+    #
+    local awsvault_profile=$1
+    #
+    # Mask profile name from argument list
+    #
+    shift
+    #
+    # Execute chamber via 'aws-vault exec'
+    #
+    awsve $awsvault_profile -- chamber $@
 }
