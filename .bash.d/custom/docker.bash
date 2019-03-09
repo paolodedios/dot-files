3b5bb1b88882fdbc9861e9b9a7d7cc9336a5560d
Paolo de Dios <paolodedios@gmail.com>
2019-03-08 21:18:53 -0800

    Reimplement repository check from get_ecr_repository_policy to describe_ecr_repository since read-only ECR users are granted ecr:DescribeRepository but not ecr:GetRepositoryPolicy

--- .bash.d/custom/docker.bash
+++ .bash.d/custom/docker.bash
@@ -248,17 +248,17 @@ function pull_image_from_aws_ecr()
 }
 
 
-function get_ecr_repository_policy()
+function describe_ecr_repository()
 {
-    local repository_policy=$(aws ecr get-repository-policy --region $AWS_SERVICE_REGION          \
-                                                            --registry-id $AWS_ACCOUNT_NUMBER     \
-                                                            --repository-name $1)
+    local repository_info=$(aws ecr describe-repositories --region $AWS_SERVICE_REGION          \
+                                                          --registry-id $AWS_ACCOUNT_NUMBER     \
+                                                          --repository-names $1)
     if [ $? -ne 0 ]; then
-        error "Failed to get repository policy for '${1}' for registry with id '$AWS_ACCOUNT_NUMBER'"
+        error "Failed to get repository info for '${1}' for registry with id '$AWS_ACCOUNT_NUMBER'"
         return 1
     fi
 
-    echo "$repository_policy"
+    echo "$repository_info"
     return 0
 }
 
